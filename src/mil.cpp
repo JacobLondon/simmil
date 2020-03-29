2bd5d4cd9766c5e29a0b852c339734259dda6b9e
Jacob <jelondon12@gmail.com>
2020-03-28 18:15:36 -0700

    Drawing tiles and shapes

--- src/mil.cpp
+++ src/mil.cpp
@@ -1,51 +1,113 @@
+#include <vector>
 #include "modules.hpp"
 
 using namespace pse;
 
-constexpr int WORLDSIZ = 10;
+struct Tiles {
+    int grass = -1;
+    int tent = -1;
+};
 
-struct {
-    int tile_grass = -1;
-    int buildings_tent = -1;
-} Options;
+struct WorldData {
+    Context& ctx;
+    std::vector<std::vector<int>> world{};
+    IVec2 tile_size;
+    IVec2 origin;
+    Tiles tiles;
+    size_t world_height;
+    size_t world_width;
 
-Context *Ctx = nullptr;
-int World[WORLDSIZ][WORLDSIZ] = { 0 };
-IVec2 TileSize;
-IVec2 Origin;
+    WorldData(Context& ctx, size_t height, size_t width);
+    void setup();
+    void update();
+    IVec2 to_screen(int wx, int wy)
+    {
+        return IVec2{
+            (origin.x * tile_size.x) + (wx - wy) * (tile_size.x / 2),
+            (origin.y * tile_size.y) + (wx + wy) * (tile_size.y / 2)
+        };
+    }
+};
 
-static IVec2 to_screen(int wx, int wy)
+WorldData::WorldData(Context& ctx, size_t height, size_t width)
+: ctx{ctx}, tile_size{90, 45}, origin{width / 2, 1}, world_height(height), world_width(width)
 {
-    return IVec2{
-        (Origin.x * TileSize.x) + (wx - wy) * (TileSize.x / 2),
-        (Origin.y * TileSize.y) + (wx + wy) * (TileSize.y / 2)
-    };
+
 }
+
+void WorldData::setup()
+{
+    tiles.grass = ctx.load_image("assets/tile_grass.png");
+    tiles.tent = ctx.load_image("assets/buildings_tent.png");
+    for (int i = 0; i < world_height; i++) {
+        world.emplace_back(world_width, tiles.grass);
+    }
+}
+
+void WorldData::update()
+{
+    IVec2 mouse{ ctx.mouse.x, ctx.mouse.y };
+    IVec2 mouse_cell{ mouse.x / tile_size.x, mouse.y / tile_size.y };
+    IVec2 mouse_offset{ mouse.x % tile_size.x, mouse.y % tile_size.y };
+    IVec2 mouse_selected{
+        (mouse_cell.y - origin.y) + (mouse_cell.x - origin.x),
+        (mouse_cell.y - origin.y) - (mouse_cell.x - origin.x)
+    };
+
+    // check if the mouse is in any of the corners of the current rectangle
+    // selected to choose the correct world tile
+    const IVec2 topleft{ 0, 0 };
+    const IVec2 topmiddle{ tile_size.x / 2, 0 };
+    const IVec2 topright{ tile_size.x, 0 };
+    const IVec2 middleleft{ 0, tile_size.y / 2 };
+    const IVec2 middleright{ tile_size.x, tile_size.y / 2 };
+    const IVec2 botleft{ 0, tile_size.y };
+    const IVec2 botmiddle{ tile_size.x / 2, tile_size.y };
+    const IVec2 botright{ tile_size.x, tile_size.y };
+    Triangle2 upleft{ topleft, topmiddle, middleleft };
+    Triangle2 upright{ topmiddle, topright, middleright };
+    Triangle2 downleft{ middleleft, botmiddle, botleft };
+    Triangle2 downright{ botmiddle, middleright, botright };
+
+    if (point_in_triangle(mouse_offset, upleft)) {
+        mouse_selected.add(-1, 0);
+    }
+    else if (point_in_triangle(mouse_offset, upright)) {
+        mouse_selected.add(0, -1);
+    }
+    else if (point_in_triangle(mouse_offset, downleft)) {
+        mouse_selected.add(0, 1);
+    }
+    else if (point_in_triangle(mouse_offset, downright)) {
+        mouse_selected.add(1, 0);
+    }
+
+    for (int wy = 0; wy < world_height; wy++) {
+        for (int wx = 0; wx < world_width; wx++) {
+            IVec2 screen = to_screen(wx, wy);
+
+            int& id = world[wy][wx];
+            ctx.draw_image(id, SDL_Rect{ screen.x, screen.y, tile_size.x, tile_size.y });
+        }
+    }
+
+    if (mouse_selected.x >= 0 && mouse_selected.x < world_width && mouse_selected.y >= 0 && mouse_selected.y < world_height) {
+        IVec2 selected_screen = to_screen(mouse_selected.x, mouse_selected.y);
+        ctx.draw_image(tiles.tent, SDL_Rect{ selected_screen.x, selected_screen.y, tile_size.x, tile_size.y });
+    }
+
+    //ctx.draw_rect(Red, SDL_Rect{ mouse_cell.x * tile_size.x, mouse_cell.y * tile_size.y, tile_size.x, tile_size.y });
+}
+
+WorldData *world;
 
 void simmil_setup(Context& ctx)
 {
-    Options.tile_grass = ctx.load_image("assets/tile_grass.png");
-    Options.buildings_tent = ctx.load_image("assets/buildings_tent.png");
-    Ctx = &ctx;
-    TileSize = IVec2{ 90, 45 };
-    Origin = IVec2{ WORLDSIZ / 2, 1 };
-
-    for (int i = 0; i < WORLDSIZ; i++) {
-        for (int j = 0; j < WORLDSIZ; j++) {
-            World[i][j] = Options.tile_grass;
-        }
-    }
-    World[2][4] = Options.buildings_tent;
+    world = new WorldData(ctx, 10, 10);
+    world->setup();
 }
 
 void simmil_update(Context& ctx)
 {
-    for (int wy = 0; wy < WORLDSIZ; wy++) {
-        for (int wx = 0; wx < WORLDSIZ; wx++) {
-            IVec2 screen_coord = to_screen(wx, wy);
-
-            int& id = World[wy][wx];
-            ctx.draw_image(id, SDL_Rect{ screen_coord.x, screen_coord.y, TileSize.x, TileSize.y });
-        }
-    }
+    world->update();
 }
